f485a11f58f73a535e2dd0384ccca0a9bff35754
Abhishek Kumar Singh <abhisheks02129@gmail.com>
2025-03-03 18:49:28 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,337 @@
+import {
+ 
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  nextJs,
+  redux,
+  tailwind,
+  angular,
+  nodejs,
+  mongodb,
+  java,
+  mysql,
+  git,
+  
+  nickelfox,
+  newgen,
+  
+  aptihealthWeb,
+  aptihealthApp,
+  zealWeb,
+  zealApp,
+  rollWeb,
+  rollApp,
+  linkedIn,
+  github,
+} from "../assets";
+
+const navigationPaths = {
+  home: "/",
+  about: "about",
+  work: "work",
+  contact: "contact",
+};
+
+export const navLinks = [
+  {
+    id: navigationPaths.about,
+    title: "About",
+  },
+  {
+    id: navigationPaths.work,
+    title: "Work",
+  },
+  {
+    id: navigationPaths.contact,
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "React Developer",
+    icon: web,
+  },
+  {
+    title: "Next Developer",
+    icon: web,
+  },
+  {
+    title: "NodeJs Developer",
+    icon: backend,
+  },
+  {
+    title: "Javascript Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Next JS",
+    icon: nextJs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Angular",
+    icon: angular,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Java",
+    icon: java,
+  },
+  {
+    name: "MySQL",
+    icon: mysql,
+  },
+  {
+    name: "Git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "Frontend Developer student ",
+    company_name: "Self-employed",
+    company_website: "https://proximus.surge.sh/index.html",
+    icon: nickelfox,
+    iconBg: "#E6DEDD",
+    date: "Jan 2023 - Jan 2025",
+    points: [
+      "Spearheaded the development of a fully responsive website for the college coding society.",
+      "Led coding sessions, actively sharing knowledge and insights to enrich members' programming skills and understanding.",
+      "Organized informative sessions on cutting-edge technologies, including JavaScript and ReactJs.",
+      "Curated and designed challenging coding problems for college competitions.",
+    ],
+  },
+  {
+    title: "Frontend Developer Intern",
+    company_name: "Prodigy InfoTech",
+    company_website: "https://prodigyinfotech.dev/",
+    icon: newgen,
+    iconBg: "#E6DEDD",
+    date: "01,Feb. 2025 - 28,Feb. 2021",
+    points: [
+      "Spearheaded the development of a various tasks assigned during the internship.",
+      "Build and learned new technologies such as TensorFlow.",
+      "Organized information on cutting edge AI features into different applications.",
+      "Curated and designed various designs during the internship.",
+    ],
+  },
+];
+
+const projects = [
+  {
+    name: "Weather Application ",
+    description:
+      "Revolutionizing mental healthcare in New York State with a user-friendly platform for personalized therapy and progress tracking. Provides the digital gateway to comprehensive behavioral healthcare.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: aptihealthWeb,
+    hosted_link: "https://www.aptihealth.com/",
+  },
+  {
+    name: "Aptihealth App",
+    description:
+      "Access top-notch mental healthcare anytime, anywhere in New York State with our innovative app. Track your progress, schedule appointments, and connect with expert therapists for personalized care.",
+    tags: [
+      {
+        name: "react-native",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "sendbird",
+        color: "green-text-gradient",
+      },
+      {
+        name: "twilio",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: aptihealthApp,
+    hosted_link: "https://apps.apple.com/us/app/aptihealth/id1477170874",
+  },
+  {
+    name: "Zeal Web",
+    description:
+      "The Zeal website elevates the experience of enjoying activities with trusted friends, fostering connections and making every interaction exciting.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "firebase",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: zealWeb,
+    hosted_link: "https://getzeal.co/",
+  },
+  {
+    name: "Zeal App",
+    description:
+      "Discover a world of activities with friends through Zeal, an engaging platform. Create events, post updates, chat, and enjoy in-person activities hassle-free. Boost your social life with Zeal!",
+    tags: [
+      {
+        name: "react-native",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "sendgrid",
+        color: "green-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: zealApp,
+    hosted_link:
+      "https://apps.apple.com/us/app/zeal-activities-with-friends/id6446917140",
+  },
+  {
+    name: "Roll Web",
+    description:
+      "The Roll website is the gateway to the future of decentralized communities and digital assets. It allows users to seamlessly earn, redeem, send, and trade social tokens.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+      {
+        name: "formik",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: rollWeb,
+    hosted_link: "https://app.tryroll.com/",
+  },
+  {
+    name: "Roll App",
+    description:
+      "Join the future of decentralized communities and digital assets with Roll. Earn, redeem, send, and trade social tokens on this innovative platform. Experience transparent transactions and captivating UI design.",
+    tags: [
+      {
+        name: "react-native-web",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+      {
+        name: "storybook",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: rollApp,
+    hosted_link:
+      "https://play.google.com/store/apps/details?id=com.roll&hl=en_GB",
+  },
+];
+
+const personalInfo = {
+  name: "Abhishek",
+  fullName: "Abhishek Kumar Singh",
+  email: "singhabhishek021202@gmail.com",
+  role: "Full Stack Developer",
+  about: `I'm a skilled full stack developer with experience in TypeScript and
+  JavaScript, and expertise in frameworks like React.Js,
+  Node.js etc. I'm a quick learner and collaborate closely with clients to
+  create efficient, scalable, and user-friendly solutions that solve
+  real-world problems. Let's work together to bring your ideas to life!`,
+  projectsIntro: `Following projects showcases my skills and experience through
+  real-world examples of my work. Each project is briefly described with
+  live demos. It reflects my ability to solve complex problems, work
+  with different technologies, and manage projects effectively.`,
+};
+
+const publicUrls = {
+  resume:
+    "https://drive.google.com/file/d/1vmiPiCLaCuqM0uuDcXQlAd8pY8yBDEOU/view?usp=sharing",
+  socialProfiles: {
+    linkedin: {
+      title: "linkedin",
+      link: "https://www.linkedin.com/in/abhishekkumarsingh17/",
+      icon: linkedIn,
+    },
+    github: {
+      title: "github",
+      link: "https://github.com/abhishek02122002",
+      icon: github,
+    },
+  },
+};
+
+export {
+  services,
+  technologies,
+  experiences,
+  projects,
+  navigationPaths,
+  personalInfo,
+  publicUrls,
+};
